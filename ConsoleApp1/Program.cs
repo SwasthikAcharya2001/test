5e5e58211928bf294e149c02b9203c94c1c01371
DEV Team <devteam@pct.com>
2023-08-07 12:12:46 +0530

    edited

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("HEllo world");
+            Console.WriteLine("2nd line");
         }
     }
 }
